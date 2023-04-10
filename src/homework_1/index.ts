7a76a25a6fa282c3e6df1982f26ebc9977ec3dac
dnotrad <lentsd@bk.ru>
2023-04-10 22:05:45 +0300

    add first lesson

--- src/homework_1/index.ts
+++ src/homework_1/index.ts
@@ -1,3 +1,10 @@
-let number: number = 1;
+import { createBitGetter } from '../lib/bit-getter';
 
-console.log(1);
+const bitGetter = createBitGetter(new Uint8Array([0b1110, 0b1101, 0b1111]));
+
+console.log(bitGetter.get(0, 1)); // 1
+console.log(bitGetter.get(1, 1)); // 0
+console.log(bitGetter.get(2, 0)); // 1
+
+bitGetter.set(1, 0, 0);
+console.log(bitGetter.get(1, 0)); // 0
